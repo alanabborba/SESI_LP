64dd90fd7d1858f4bd89801c108d35b3c0aa69a0
Alana Bosa Borba <alana.b.borba@aluno.senai.br>
2026-04-28 08:27:35 -0300

    exercícios 28/04

---
+++ AULA_05/EXERCÍCIO.11.0.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    float numeros[10]; 
+    int i, j;
+    int temp;
+
+    printf("DIGITE 10 NÚMEROS:\n");
+    for(i = 0; i < 10; i++) {
+        printf("NÚMERO %d: ", i + 1);
+        scanf("%f", &numeros[i]);  }
+    
+    for(i = 0; i < 9; i++) {           
+        for(j = i + 1; j < 10 - i; j++) {  
+            if(numeros[i] < numeros[j]) {
+                temp = numeros [i];
+                numeros[i] = numeros[j];
+                numeros[j] = temp;
+            } } }
+
+    printf("\nVETOR EM ORDEM CRESCENTE:\n");
+    for(i = 0; i < 10; i++) {
+        printf("%.2f ", numeros[i]); 
+    
+        
+    } printf("\n"); }
+    
